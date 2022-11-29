ba2200e94bb2b71e3d3a281fc137c1425633bc7b
remy000 <dukundaneremy2001@gmail.com>
2022-11-29 17:48:10 +0200

    cheapestproduct.js

---
+++ TechUp-Skill-Challenge/javascript/cheapestproduct.js
@@ -0,0 +1,21 @@
+const items = [
+    {name: 'Bike',     price: 100  },
+    {name: 'TV',       price: 200  },
+    {name: 'Album',    price: 10   },
+    {name: 'Book',     price: 5    },
+    {name: 'Phone',    price: 500  },
+    {name: 'Computer', price: 1000 }
+]
+
+var i = 1
+var j = 0 
+holder = items[0].price
+  while(i<6){
+    if (items[i].price < holder){
+    j = i
+    i++
+  }else{
+    i++
+  }
+  }
+console.log(`The cheapest item is a ${items[j].name} and it costs ${items[j].price}`)
